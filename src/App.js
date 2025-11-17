2604deccde5431b12b33311e449ad6af5c068192
unknown <mzuasnabarcenteno@gmail.com>
2025-11-16 23:45:55 -0500

    subiendo cambios a rama-lovable

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          veremos si funciona
+          veremos si funciona el pull request
         </a>
       </header>
     </div>
